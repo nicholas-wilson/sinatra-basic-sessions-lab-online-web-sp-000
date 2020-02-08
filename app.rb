8d444be160d5023f326413d0c98820f9c0527475
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2020-02-08 02:00:56 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -13,7 +13,7 @@ class App < Sinatra::Base
 
   post '/checkout' do
     raise params.inspect
-    session[:item] = params
+    session[:item] = params[:item]
     @session = session
     erb :item
   end
