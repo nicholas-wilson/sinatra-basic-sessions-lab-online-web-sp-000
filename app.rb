e6d6bd2c986d31fa8e1542d98a6ff8fa0de336b9
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2020-02-08 02:04:08 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  configure do
+    enable :sessions
+    set :session_secret, "item"
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  post '/checkout' do
+    # raise params.inspect
+    session[:item] = params[:item]
+    @session = session
+    erb :item
+  end
 end
